e2082a6b110ca5d003ad13131d101373a4b4345b
shivam shrivastava <shivamshrivaastava@gmail.com>
2024-03-28 13:07:08 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Menu from './Components/Menu';
+import Container from './Components/Container';
+function App() {
+  return (
+    <div className="App">
+     <Menu />
+     <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainRightBottomCard.js
@@ -0,0 +1,33 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import TopSeller from "./TopSeller";
+
+function MainRightBottomCard() {
+  return (
+    <div className="bottom_card">
+      <div className="bottomCard_name">
+        <h2>Top Seller</h2>
+        <a href="#">View More</a>
+      </div>
+
+      {TopSeller &&
+        TopSeller.map((seller) => (
+          <div className="topSeller" key={seller.id}>
+            <div className="topSellerImg">
+              <img src={seller?.imgSrc} alt="" />
+            </div>
+            <div className="topSellerName">
+              <p>
+                {seller?.seller_name} <span>{seller?.username}</span>
+              </p>
+            </div>
+            <a href="#" className="button">
+              Follow
+            </a>
+          </div>
+        ))}
+    </div>
+  );
+}
+
+export default MainRightBottomCard;
